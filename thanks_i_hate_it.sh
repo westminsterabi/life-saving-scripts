92cc3f640dcfaf09604a974ac8283ff9502aac2d
Abi Hunter <abihunter@gmail.com>
2019-05-02 18:10:19 -0500

    a little cleanup, added my favorite color scheme

--- thanks_i_hate_it.sh
+++ thanks_i_hate_it.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# install atom
 atom
 if [[ $? != 0 ]]; then
   echo "Downloading atom"
@@ -8,6 +9,7 @@ if [[ $? != 0 ]]; then
   echo $(curl $url -o atom-mac.zip)
 fi
 
+# install homebrew
 which -s brew
 if [[ $? != 0 ]] ; then
     # Install Homebrew
@@ -17,18 +19,21 @@ else
     brew update
 fi
 
+# make zsh default
 chsh -s $(which zsh)
 
+# install font icons
 brew tap caskroom/fonts
-brew cask install font-hack-nerd-font --dry-run
+brew cask install font-hack-nerd-font
 
-
+# install powerlevel9k
 ls ~/powerlevel9k
 if [[ $? != 0 ]]; then
   git clone https://github.com/bhilburn/powerlevel9k.git ~/powerlevel9k
 fi
 
 
+# configure powerlevel9k
 echo "POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(dir vcs newline status)" >> ~/.zshrc
 echo "POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=()" >> ~/.zshrc
 echo "POWERLEVEL9K_PROMPT_ADD_NEWLINE=true" >> ~/.zshrc
@@ -36,7 +41,10 @@ echo "POWERLEVEL9K_MODE='nerdfont-complete'" >> ~/.zshrc
 
 echo 'source  ~/powerlevel9k/powerlevel9k.zsh-theme' >> ~/.zshrc
 
+# make sfmono available to iterm
 cd /Applications/Utilities/Terminal.app/Contents/Resources/Fonts/
 cp *.otf ~/Library/Fonts/
 
+# get iterm and my color scheme
 brew cask install iterm2
+curl -O https://raw.githubusercontent.com/mbadolato/iTerm2-Color-Schemes/master/schemes/lovelace.itermcolors
